8a294e9251d3787d113e067d257900ff85d60370
minh tuan <minhtuandev2001@gmail.com>
2025-04-05 12:47:56 +0700

    reponsive header, component dropdow

---
+++ src/cms/components/Dropdows/Dropdows.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { useOutsideClick } from "../../../hook/useOutSideClick";
+
+interface IItem {
+  render: () => React.ReactElement;
+  icon: React.ReactElement;
+  isMobile?: boolean;
+}
+interface Props {
+  children: React.ReactNode;
+  itemMenu?: IItem[];
+}
+export default function Dropdows({ children, itemMenu }: Props) {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const closeDropdow = () => setShowMenu(false);
+  const wrapperRef = useOutsideClick({
+    isOpen: showMenu,
+    onClose: closeDropdow,
+    excludeClass: "dropdow-avatar-container", // Tắt khi không chứa class này
+  });
+  return (
+    <div
+      ref={wrapperRef}
+      onClick={() => setShowMenu(true)}
+      className="group/show text-gray-custom w-full relative text-sm"
+    >
+      {children}
+      <div
+        className={twMerge(
+          `hidden absolute top-full right-0 z-[9999] pt-2 animate-fade-down group-hover/show:block ${
+            showMenu ? "block" : ""
+          }`
+        )}
+      >
+        <div className="p-[5px] bg-white shadow-dropdow-custom w-full min-w-[200px] rounded-md">
+          {(itemMenu?.length ?? 0) > 0 &&
+            itemMenu?.map((item: IItem, index: number) => (
+              <li
+                key={index}
+                className={`flex items-center flex-nowrap gap-2 w-full cursor-pointer px-3 py-2 hover:bg-gray-custom/10 rounded-md transition-all ${
+                  item?.isMobile ? "block md:hidden" : "block"
+                }`}
+              >
+                {item?.icon}
+                {item.render()}
+              </li>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/cms/components/Headers/MainHeader.tsx
+++ src/cms/components/Headers/MainHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import logo from "../../../assets/images/logo.png";
 import avatar from "../../../assets/images/avatar.png";
 import { IoSettingsOutline } from "react-icons/io5";
@@ -7,15 +7,47 @@ import { GoBell } from "react-icons/go";
 import { FaRegQuestionCircle } from "react-icons/fa";
 import { IoChevronDownOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
+import { useOutsideClick } from "../../../hook/useOutSideClick";
+import Dropdows from "../Dropdows/Dropdows";
+import { FaRegUser } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
+
+const itemMenu = [
+  {
+    icon: <FaRegUser className="min-w-[20px]" />,
+    render: () => <p className="text-nowrap">Minh tuan</p>,
+  },
+  {
+    icon: <IoSettingsOutline className="min-w-[20px]" />,
+    render: () => <p className="text-nowrap">Cài đặt</p>,
+    isMobile: true,
+  },
+  {
+    icon: <FaRegQuestionCircle className="min-w-[20px]" />,
+    render: () => <p className="text-nowrap">Trợ giúp</p>,
+    isMobile: true,
+  },
+  {
+    icon: <IoIosLogOut size={20} className="min-w-[20px]" />,
+    render: () => <p className="text-nowrap text-red-custom">Logout</p>,
+  },
+];
 
 export default function MainHeader() {
+  const [isSearchMobileOpen, setIsSearchMobileOpen] = useState<boolean>(false);
+  const closeSearch = () => setIsSearchMobileOpen(false);
+  const wrapperRef = useOutsideClick({
+    isOpen: isSearchMobileOpen,
+    onClose: closeSearch,
+    excludeClass: "search-container", // Tắt khi không chứa class này
+  });
   return (
-    <div className="w-full bg-blue-custom text-white flex items-center justify-between py-5 px-[48px]">
+    <div className="w-full bg-blue-custom text-white flex items-center justify-between px-2 py-3  lg:py-5 lg:px-[48px]">
       <div className="flex items-center gap-x-6">
-        <div className="w-[82.77px] h-[32px]">
+        <div className="w-[72.77px] h-[22px] md:w-[82.77px] md:h-[32px]">
           <img src={logo} alt="logo" className="w-full h-full object-cover" />
         </div>
-        <ul className="flex items-center text-sm font-normal cursor-pointer ">
+        <ul className="flex items-center text-sm font-normal cursor-pointer hidden">
           <li className="py-[4px] px-2 hover:text-white/70 transition-all ">
             Danh mục
           </li>
@@ -37,8 +69,8 @@ export default function MainHeader() {
           <li className="py-[4px] px-2 hover:text-white/70">Tiện ích</li>
         </ul>
       </div>
-      <div className="flex items-center gap-x-6">
-        <div className="flex items-center gap-x-2 bg-white/10 rounded-lg p-[6px]">
+      <div className="flex items-center gap-x-4 md:gap-x-6">
+        <div className="items-center gap-x-2 bg-white/10 rounded-lg p-[6px] hidden md:flex">
           <CiSearch size={16} className="min-w-4 text-white" />
           <input
             type="text"
@@ -47,13 +79,32 @@ export default function MainHeader() {
           />
         </div>
         <div className="flex items-center gap-4">
+          <div ref={wrapperRef} className="block md:hidden">
+            <CiSearch
+              onClick={() => setIsSearchMobileOpen(true)}
+              size={20}
+              className="text-white min-w-5 cursor-pointer md:hidden"
+            />
+            <div className="block md:hidden">
+              {isSearchMobileOpen && (
+                <div className="flex items-center rounded-md p-2 fixed top-[70px] left-1/2 -translate-x-1/2 z-[9999] shadow-dropdow-custom w-[80%] border border-gray-500/30 animate-jump-in">
+                  <CiSearch size={16} className="min-w-4 text-gray-custom" />
+                  <input
+                    type="text"
+                    placeholder="Tìm kiếm"
+                    className="bg-red-200 font-thin w-full text-gray-custom pl-1"
+                  />
+                </div>
+              )}
+            </div>
+          </div>
           <IoSettingsOutline
             size={20}
-            className="text-white min-w-5 cursor-pointer"
+            className="text-white min-w-5 cursor-pointer !hidden md:!block"
           />
           <IoSettingsOutline
             size={20}
-            className="text-white min-w-5 cursor-pointer"
+            className="text-white min-w-5 cursor-pointer !hidden md:!block"
           />
           <TbMessage2 size={20} className="text-white min-w-5 cursor-pointer" />
           <div className="relative flex items-center cursor-pointer">
@@ -64,19 +115,24 @@ export default function MainHeader() {
           </div>
           <FaRegQuestionCircle
             size={20}
-            className="text-white min-w-5 cursor-pointer"
+            className="text-white min-w-5 cursor-pointer !hidden"
           />
         </div>
-        <div className="flex items-center gap-x-[4px] cursor-pointer">
-          <div className="w-10 h-10 rounded-full overflow-hidden">
-            <img
-              src={avatar}
-              alt="avatar"
-              className="w-ful h-full object-cover"
-            />
-          </div>
-          <IoChevronDownOutline size={20} className="min-w-5" />
-        </div>
+        <Dropdows
+          children={
+            <div className="flex items-center gap-x-[4px] cursor-pointer">
+              <div className="w-8 h-8 md:w-10 md:h-10 rounded-full overflow-hidden">
+                <img
+                  src={avatar}
+                  alt="avatar"
+                  className="w-ful h-full object-cover"
+                />
+              </div>
+              <IoChevronDownOutline className="min-w-3 md:min-w-5 text-white" />
+            </div>
+          }
+          itemMenu={itemMenu}
+        ></Dropdows>
       </div>
     </div>
   );
